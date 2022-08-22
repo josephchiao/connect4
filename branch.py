6cf2cf182f3db68f6fbce1dd338922eeb1e36ba9
josephchiao <chiaofeng.0118@gmail.com>
2022-08-22 11:07:16 -0700

    changed to slighly more efficienct computer movement

--- branch.py
+++ branch.py
@@ -33,7 +33,7 @@ def branch_playable(game):
     legal_moves = branch_avalibilities(game.board)
     for possible_position in legal_moves:
         branch = copy.deepcopy(game)
-        branch.player_movement(possible_position[1])
+        branch.computer_movement(possible_position)
         playable_branch.append(branch)
     
     return playable_branch
