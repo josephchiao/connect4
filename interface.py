81740ae21874b31f938239a05e1d19865e0d1a52
josephchiao <chiaofeng.0118@gmail.com>
2022-08-24 23:54:12 -0700

    reinstated player_vs_computer

--- interface.py
+++ interface.py
@@ -50,7 +50,10 @@ def player_vs_player(Game):
             print("Yellow wins")
             end = True
 
-def player_vs_computer(Game):
+def player_vs_computer():
+
+    Game = gp.Game(setup.board_generation())
+
 
     player_side = None
 
@@ -65,24 +68,26 @@ def player_vs_computer(Game):
             player_side = 1
             computer_side = 0
 
+    thetas = np.load('nn_theta_set.npz')
+    theta_1 = thetas['Theta1']
+    theta_2 = thetas['Theta2']
+    theta_3 = thetas['Theta3']
+
     end = False
-    display(Game.board)
+    display(Game.board, Game.side)
     
     while not end:
 
         if Game.side == player_side:
             Game.player_input()
 
-            end = game_interval()
 
         elif Game.side == computer_side:
-            best_line = ts.tree_search(Game, 5)
-            print(best_line)
-            new_game_state = best_line[1][1]
-            Game.board = new_game_state.board
-            Game.side = 1 - Game.side 
+            best_move = ts.tree_search(Game, 7, theta_1 = theta_1, theta_2 = theta_2, theta_3 = theta_3)
+        
+            Game.computer_movement(best_move)
 
-            end = game_interval()
+        end = game_interval(Game)
 
 def computer_vs_computer():
 
@@ -98,11 +103,12 @@ def computer_vs_computer():
     while not end:
 
         if Game.side == 0:
-            best_line = ts.tree_search(Game, 4, theta_1 = theta_1, theta_2 = theta_2, theta_3 = theta_3)
+            best_move = ts.tree_search(Game, 4, theta_1 = theta_1, theta_2 = theta_2, theta_3 = theta_3)
         elif Game.side == 1:
-            best_line = ts.tree_search(Game, 4, theta_1 = theta_1, theta_2 = theta_2, theta_3 = theta_3)
-        print(best_line)
-        Game = best_line[1][1]
+            best_move = ts.tree_search(Game, 4, theta_1 = theta_1, theta_2 = theta_2, theta_3 = theta_3)
+        print(best_move)
+
+        Game.computer_movement(best_move)
         
         end = game_interval(Game)
     
@@ -127,8 +133,8 @@ def game_interval(Game):
 # Game = gp.Game([[1, 0, 1, 0, 1, 0, 1], [0, 0, 0, None, 1, 0, None], [0, 0, 1, None, 0, 1, None], [1, 1, 1, None, 0, 0, None], [1, 1, 1, None, 0, 1, None], [0, 0, 1, None, 1, 0, None]], side= 1)
 # Game.update_win_con((5,2))
 # print(Game)
-computer_vs_computer()
-# player_vs_computer()
+# computer_vs_computer()
+player_vs_computer()
 # player_vs_player()
 # best_line = ts.tree_search(Game, 4)
 # print(f"eval: {best_line[0]}")
